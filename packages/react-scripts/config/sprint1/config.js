5654adcb195a4fec8a736983ceebafc2c8c3ba95
Senthil Ramachandran <senthil@Sprint1Software.com>
2022-03-04 12:42:14 -0600

    Resolve @sprint1/pkg

--- packages/react-scripts/config/sprint1/config.js
+++ packages/react-scripts/config/sprint1/config.js
@@ -19,6 +19,24 @@ function getNodeModulePathsToTranspile() {
   }
 }
 
+function getNodeModulePathsToResolve() {
+  if (
+    packageJsonObject.typeScriptNodeModulePathsToTranspile &&
+    Array.isArray(packageJsonObject.typeScriptNodeModulePathsToTranspile)
+  ) {
+    const pathsToInclude =
+      packageJsonObject.typeScriptNodeModulePathsToTranspile.map(
+        modulePathInPackageJson => {
+          return path.resolve('node_modules', modulePathInPackageJson, 'src');
+        }
+      );
+    return pathsToInclude;
+  } else {
+    return [];
+  }
+}
+
 module.exports = {
   getNodeModulePathsToTranspile: getNodeModulePathsToTranspile,
+  getNodeModulePathsToResolve: getNodeModulePathsToResolve,
 };
